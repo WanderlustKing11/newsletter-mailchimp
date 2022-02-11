1ad87a1fd11530bc48cbdf94b9e3d7176964f9bc
Douglas Horville <64605163+WanderlustKing11@users.noreply.github.com>
2022-02-10 22:49:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); // "public" is our static folder, allows us to render our css on browswer via server
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// app.use(express.urlencoded({extended: true}));  // Parse URL-encoded bodies
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html");
+app.get("/", function(req, res) { // Using our home route "/"
+  res.sendFile(__dirname + "/signup.html"); // This is the get route to our signup page, test on browswer at localhost:3000
 });
 
 app.post("/", function(req, res) {
 
-  var firstName = req.body.fName
-  var lastName = req.body.lName
-  var email = req.body.email
-
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
   console.log(firstName, lastName, email);
 
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us**region code here**.api.mailchimp.com/3.0/lists/**List ID**";
+
+  const options = {
+    method: "POST",
+    auth: "**string:API Key************"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  // request.write(jsonData);
+  request.end();
+
+});
+
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
